5f0725585fe53c5c1ed39a2c1ba0ccb7bb681ff4
HERIK238 <herikbernalgomez@gmail.com>
2025-09-07 12:23:45 -0500

    cambiando nombres

--- ui/sidebar.php
+++ ui/sidebar.php
@@ -77,7 +77,7 @@
         <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 280px">
             <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                 <img src="../assets/icons/icon-48x48.png" alt="" width="32" height="32" class="rounded-circle me-2" />
-                <span class="fs-4">roepard-login</span>
+                <span class="fs-4">Survival</span>
             </a>
             <hr />
             <ul class="nav nav-pills flex-column mb-auto">
@@ -86,7 +86,7 @@
                         <svg class="bi pe-none me-2" width="16" height="16" aria-hidden="true">
                             <use xlink:href="#home"></use>
                         </svg>
-                        Homelab
+                        Survival
                     </a>
                 </li>
                 <li>
