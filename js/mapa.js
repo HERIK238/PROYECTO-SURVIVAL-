4d857cc3b1180ad6ec81906a0b1b57bd03d4a28e
HERIK238 <herikbernalgomez@gmail.com>
2025-09-10 15:28:04 -0500

    contribucion a lo de jero

--- js/mapa.js
+++ js/mapa.js
@@ -1,5 +1,5 @@
 // Eliminar cajas al hacer click con el crosshair
-document.querySelector("a-scene").addEventListener("click", function (evt) {
+document.querySelector("#player").addEventListener("click", function (evt) {
   const intersected = evt.detail.intersectedEl;
   if (intersected && intersected.classList.contains("clickable")) {
     intersected.parentNode.removeChild(intersected);
