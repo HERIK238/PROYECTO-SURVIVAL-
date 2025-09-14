48f6c44ed1ba087f32858f247030209acc29ac14
jerorjuebal123 <orjuelaballesterosjeronimo@gmail.com>
2025-09-14 16:25:42 -0500

    temporizador

--- js/mapa.js
+++ js/mapa.js
@@ -1,7 +1,43 @@
+
+let totalCajas = 6; 
+let cajasAgarradas = 0;
+let tiempoRestante = 7000; // segundos
+let intervalo;
+
+// Mostrar HUD
+const contadorEl = document.getElementById("contador");
+const tiempoEl = document.getElementById("tiempo");
+
+// Iniciar temporizador
+function iniciarTimer() {
+  intervalo = setInterval(() => {
+    tiempoRestante--;
+    tiempoEl.textContent = "Tiempo restante: " + tiempoRestante + "s";
+
+    if (tiempoRestante <= 0) {
+      clearInterval(intervalo);
+      alert("¡Tiempo agotado! Perdiste 😢");
+      location.reload(); // reinicia el juego
+    }
+  }, 1000);
+}
+
 // Eliminar cajas al hacer click con el crosshair
 document.querySelector("#player").addEventListener("click", function (evt) {
   const intersected = evt.detail.intersectedEl;
   if (intersected && intersected.classList.contains("clickable")) {
     intersected.parentNode.removeChild(intersected);
+
+    cajasAgarradas++;
+    contadorEl.textContent = "Cajas: " + cajasAgarradas + " / " + totalCajas;
+
+    if (cajasAgarradas >= totalCajas) {
+      clearInterval(intervalo);
+      alert("¡Ganaste! 🎉 Has recogido todas las cajas");
+      location.reload();
+    }
   }
 });
+
+// Inicia el temporizador cuando se cargue la escena
+window.addEventListener("load", iniciarTimer);
